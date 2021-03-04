7021651472bf8a6cf486fcd6dd6155165a557cd1
AlexisEche <alexis.eche58@gmail.com>
2021-03-03 23:50:08 -0500

    Change root

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
 import { BrowserRouter as Router,Switch, Route } from "react-router-dom";
 import Home from './components/pages/Home'
 import DetailOfCourse from "./components/pages/DetailOfCourse";
